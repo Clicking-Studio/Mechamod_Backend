fcdfc3bb54b38ac7a5f79997448543568002aa18
Mahmoud <tkhattab1999@gmail.com>
2024-02-07 20:02:54 -0500

    removed requires

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ const express = require("express");
 const app = express();
 const pool = require("./db");
 const cors = require("cors");
-const multer = require('multer');
-const path = require('path');
+
 
 const PORT = process.env.PORT || 3000;
 
