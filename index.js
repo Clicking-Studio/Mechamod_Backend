e0574af405018e11892d1d121ac4dd52369a4757
Mahmoud <tkhattab1999@gmail.com>
2024-03-04 11:46:53 -0500

    Vercel configuration

--- index.js
+++ index.js
@@ -4,25 +4,32 @@ const pool = require("./db");
 const cors = require("cors");
 const multer = require("multer");
 const bodyParser = require("body-parser");
+
+if (process.env.NODE_ENV !== "PRODUCTION") {
+	require("dotenv").config();
+}
+
+// Load environment variables from .env file
 const uploadFileToS3 = require("./AWS_S3_OPERATIONS/s3FileUploader");
+
+console.log(process.env.REGION);
 
 const PORT = process.env.PORT || 3000;
 
 const storage = multer.memoryStorage();
 const upload = multer({
-  storage: storage,
-  limits: {
-    fileSize: 50 * 1024 * 1024, // 50 MB limit
-  }
+	storage: storage,
+	limits: {
+		fileSize: 50 * 1024 * 1024, // 50 MB limit
+	},
 }).array("files", 2); // Allow up to 2 files
 
 app.use(cors());
-app.use(express.json({ limit: '50mb' })); // Increase JSON request body limit
+app.use(express.json({ limit: "50mb" })); // Increase JSON request body limit
 // Increase payload size limit
-app.use(bodyParser.json({ limit: '50mb' }));
-app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
-app.use('/uploads', express.static('uploads'));
-
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
+app.use("/uploads", express.static("uploads"));
 
 // Default endpoint
 app.get("/", (req, res) => {
@@ -60,27 +67,27 @@ app.get("/keycaps/:id", async (req, res) => {
 
 // Add a new keycap
 app.post("/keycaps", upload, async (req, res) => {
-  try {
-    let imageobj = {}; // will contain the URL and the name of the image file that will be uploaded to S3 bucket
-    let stlfileobj = {}; // will contain the URL and the name of the STL file that will be uploaded to S3 bucket
-    
-    for (const file of req.files) {
-      const uploadedFile = await uploadFileToS3(file);
+	try {
+		let imageobj = {}; // will contain the URL and the name of the image file that will be uploaded to S3 bucket
+		let stlfileobj = {}; // will contain the URL and the name of the STL file that will be uploaded to S3 bucket
 
-      if (file.mimetype.startsWith("image")) {
-        imageobj = uploadedFile;
-      } else {
-        stlfileobj = uploadedFile;
-      }
-    }
+		for (const file of req.files) {
+			const uploadedFile = await uploadFileToS3(file);
 
-    const { name, price, description } = req.body;
-    const imagePath = imageobj.url; // Path to the uploaded image
-    const stlPath = stlfileobj.url; // Path to the uploaded stl
+			if (file.mimetype.startsWith("image")) {
+				imageobj = uploadedFile;
+			} else {
+				stlfileobj = uploadedFile;
+			}
+		}
+
+		const { name, price, description } = req.body;
+		const imagePath = imageobj.url; // Path to the uploaded image
+		const stlPath = stlfileobj.url; // Path to the uploaded stl
 
 		const newKeycap = await pool.query(
 			"INSERT INTO keycap (name, price, description, image_path, stl_path) VALUES ($1, $2, $3, $4, $5) RETURNING *",
-			[name, price, description, imagePath, stlPath]
+			[name, price, description, imagePath, stlPath],
 		);
 
 		res.json(newKeycap.rows[0]);
@@ -92,35 +99,35 @@ app.post("/keycaps", upload, async (req, res) => {
 });
 
 app.put("/keycaps/:id", upload, async (req, res) => {
-  try {
-  let imageobj = {}; // will contain the URL and the name of the image file that will be uploaded to S3 bucket
-  let stlfileobj = {}; // will contain the URL and the name of the STL file that will be uploaded to S3 bucket
-  
-      for (const file of req.files) {
-        const uploadedFile = await uploadFileToS3(file);
-  
-        if (file.mimetype.startsWith("image")) {
-          imageobj = uploadedFile;
-        } else {
-          stlfileobj = uploadedFile;
-        }
-      }
-      const { id } = req.params;
-      const { name, price, description, order_position } = req.body;
-      const imagePath = imageobj.url; // Path to the uploaded image
-      const stlPath = stlfileobj.url; // Path to the uploaded stl
+	try {
+		let imageobj = {}; // will contain the URL and the name of the image file that will be uploaded to S3 bucket
+		let stlfileobj = {}; // will contain the URL and the name of the STL file that will be uploaded to S3 bucket
 
-      const updateKeycap = await pool.query(
-          "UPDATE keycap SET name = $1, price = $2, description = $3, order_position = $4, image_path = $5, stl_path = $6 WHERE keycap_id = $7 RETURNING *",
-          [name, price, description, order_position, imagePath, stlPath, id],
-      );
+		for (const file of req.files) {
+			const uploadedFile = await uploadFileToS3(file);
 
-      res.json(updateKeycap.rows[0]);
-      console.log("Updating keycap");
-  } catch (err) {
-      console.log(err.message);
-      res.status(500).json({ error: "Internal server error" });
-  }
+			if (file.mimetype.startsWith("image")) {
+				imageobj = uploadedFile;
+			} else {
+				stlfileobj = uploadedFile;
+			}
+		}
+		const { id } = req.params;
+		const { name, price, description, order_position } = req.body;
+		const imagePath = imageobj.url; // Path to the uploaded image
+		const stlPath = stlfileobj.url; // Path to the uploaded stl
+
+		const updateKeycap = await pool.query(
+			"UPDATE keycap SET name = $1, price = $2, description = $3, order_position = $4, image_path = $5, stl_path = $6 WHERE keycap_id = $7 RETURNING *",
+			[name, price, description, order_position, imagePath, stlPath, id],
+		);
+
+		res.json(updateKeycap.rows[0]);
+		console.log("Updating keycap");
+	} catch (err) {
+		console.log(err.message);
+		res.status(500).json({ error: "Internal server error" });
+	}
 });
 
 app.delete("/keycaps/:id", async (req, res) => {
@@ -186,7 +193,6 @@ app.delete("/cart/:id", async (req, res) => {
 		console.error(err.message);
 	}
 });
-
 
 app.listen(PORT, () => {
 	console.log(`server is online at ${PORT}`);
