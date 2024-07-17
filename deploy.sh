b32c98619cd7d23d0e7187e0c4353bfab370e39e
MUmarRasheed <sfsumar@gmail.com>
2024-07-17 20:17:06 +0500

    path fixed

--- deploy.sh
+++ deploy.sh
@@ -9,7 +9,7 @@ LOCAL_REPO_PATH="/home/ec2-user/Mechamod_Backend"
 BRANCH_NAME="main"
 
 # Full path to pm2
-PM2_PATH="/root/.nvm/versions/node/v18.16.1/bin/pm2"
+PM2_PATH="/root/.nvm/versions/node/v18.16.1/bin/"
 
 # Function to check if new commits are available in the specified branch
 check_for_new_commits() {
@@ -43,11 +43,11 @@ fetch_and_restart_pm2() {
         exit 1
     fi
 
-    # Pull latest changes from the specified branch using fast-forward only
+    # Pull latest changes from the specified branch
     git pull origin "$BRANCH_NAME" --no-rebase
 
     # Restart pm2 using the full path
-    $PM2_PATH restart 6
+    $PM2_PATH pm2 restart 6
 }
 
 # Main function
